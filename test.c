76ed5483540854c203e5ae3db82ba18d782f3169
tigerlee3 <tigerlee2@naver.com>
2015-11-24 14:41:36 +0900

    initial comit

--- test.c
+++ test.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+#include <time.h>
+#include <stdlib.h>
 
+#define LEFT 75
+#define RIGHT 77
+#define ROTATE 72
+#define UP_ROTATE 100
+#define DOWN 80
+#define SPACE 32
 
-int main()
+void 
